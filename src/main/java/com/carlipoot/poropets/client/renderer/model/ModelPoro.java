adf2f6090b8c6ed38c7cedd23e95d5e65af36af0
Carlipoot <carl_hansen@hotmail.com>
2014-07-15 00:46:54 +1000

    Fixed some model issues. Playing with Entity fields

--- src/main/java/com/carlipoot/poropets/client/renderer/model/ModelPoro.java
+++ src/main/java/com/carlipoot/poropets/client/renderer/model/ModelPoro.java
@@ -3,8 +3,11 @@ package com.carlipoot.poropets.client.renderer.model;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.model.ModelCreeper;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
+import net.minecraft.util.MathHelper;
+import org.lwjgl.opengl.GL11;
 
 @SideOnly(Side.CLIENT)
 public class ModelPoro extends ModelBase{
@@ -17,86 +20,114 @@ public class ModelPoro extends ModelBase{
     private ModelRenderer footBR;
     private ModelRenderer hornBaseR;
     private ModelRenderer hornBaseL;
-    private ModelRenderer hornTipL;
-    private ModelRenderer hornTipR;
+//    private ModelRenderer hornTipL;
+//    private ModelRenderer hornTipR;
 
     // Texture fields
     private int textureWidth = 64;
     private int textureHeight = 32;
 
+    private float scale = 0F;
+
     public ModelPoro() {
         body = new ModelRenderer(this, 1, 11);
-        body.addBox(-2F, -2F, -2F, 4, 4, 4, 0F);
-        body.setRotationPoint(0F, 0F, 0F);
-        body.rotateAngleX = 0F;
-        body.rotateAngleY = 0F;
-        body.rotateAngleZ = 0F;
+        body.addBox(-2F, -2F, -2F, 4, 4, 4, scale);
+        body.setRotationPoint(0F, 21F, 0F);
+        setRotation(body, 0F, 0F, 0F);
+
         footBL = new ModelRenderer(this, 0, 25);
-        footBL.addBox(-1F, -1F, -1F, 2, 2, 2, 0F);
-        footBL.setRotationPoint(2F, 2F, 1.466667F);
-        footBL.rotateAngleX = 0F;
-        footBL.rotateAngleY = 0F;
-        footBL.rotateAngleZ = 0F;
+        footBL.addBox(-1F, -1F, -1F, 2, 2, 2, scale);
+        footBL.setRotationPoint(1.5F, 2F, 1.466667F);
+        setRotation(footBL, 0F, 0F, 0F);
+        body.addChild(footBL);
+
         footFL = new ModelRenderer(this, 0, 20);
-        footFL.addBox(-1F, -1F, -1F, 2, 2, 2, 0F);
-        footFL.setRotationPoint(2F, 2F, -1.5F);
-        footFL.rotateAngleX = 0F;
-        footFL.rotateAngleY = 0F;
-        footFL.rotateAngleZ = 0F;
+        footFL.addBox(-1F, -1F, -1F, 2, 2, 2, scale);
+        footFL.setRotationPoint(1.5F, 2F, -1.5F);
+        setRotation(footFL, 0F, 0F, 0F);
+        body.addChild(footFL);
+
         footFR = new ModelRenderer(this, 10, 20);
-        footFR.addBox(-1F, -1F, -1F, 2, 2, 2, 0F);
-        footFR.setRotationPoint(-2F, 2F, -1.5F);
-        footFR.rotateAngleX = 0F;
-        footFR.rotateAngleY = 0F;
-        footFR.rotateAngleZ = 0F;
+        footFR.addBox(-1F, -1F, -1F, 2, 2, 2, scale);
+        footFR.setRotationPoint(-1.5F, 2F, -1.5F);
+        setRotation(footFR, 0F, 0F, 0F);
+        body.addChild(footFR);
+
         footBR = new ModelRenderer(this, 10, 25);
-        footBR.addBox(-1F, -1F, -1F, 2, 2, 2, 0F);
-        footBR.setRotationPoint(-2F, 2F, 1.5F);
-        footBR.rotateAngleX = 0F;
-        footBR.rotateAngleY = 0F;
-        footBR.rotateAngleZ = 0F;
+        footBR.addBox(-1F, -1F, -1F, 2, 2, 2, scale);
+        footBR.setRotationPoint(-1.5F, 2F, 1.5F);
+        setRotation(footBR, 0F, 0F, 0F);
+        body.addChild(footBR);
+
         hornBaseR = new ModelRenderer(this, 10, 7);
-        hornBaseR.addBox(-0.5F, -2F, -1F, 1, 2, 1, 0F);
-        hornBaseR.setRotationPoint(-1F, -1F, -1F);
-        hornBaseR.rotateAngleX = -0.5235988F;
-        hornBaseR.rotateAngleY = 0F;
-        hornBaseR.rotateAngleZ = 0F;
+        hornBaseR.addBox(-0.5F, -2F, -0.5F, 1, 2, 1, scale);
+        hornBaseR.setRotationPoint(-1F, -1.5F, -1.5F);
+        setRotation(hornBaseR, degToRad(-55F), 0F, degToRad(-20F));
+        body.addChild(hornBaseR);
+
         hornBaseL = new ModelRenderer(this, 3, 7);
-        hornBaseL.addBox(-0.5F, -2F, -1F, 1, 2, 1, 0F);
-        hornBaseL.setRotationPoint(1F, -1F, -1F);
-        hornBaseL.rotateAngleX = -0.5235988F;
-        hornBaseL.rotateAngleY = 0F;
-        hornBaseL.rotateAngleZ = 0F;
-        hornTipL = new ModelRenderer(this, 3, 4);
-        hornTipL.addBox(-0.5F, -0.5F, -0.5F, 1, 1, 1, 0F);
-        hornTipL.setRotationPoint(1F, -3F, -0.5F);
-        hornTipL.rotateAngleX = 0F;
-        hornTipL.rotateAngleY = 0F;
-        hornTipL.rotateAngleZ = 0F;
-        hornTipR = new ModelRenderer(this, 10, 4);
-        hornTipR.addBox(0F, 0F, 0F, 1, 1, 1, 0F);
-        hornTipR.setRotationPoint(0F, 0F, 0F);
-        hornTipR.rotateAngleX = 0F;
-        hornTipR.rotateAngleY = 0F;
-        hornTipR.rotateAngleZ = 0F;
+        hornBaseL.addBox(-0.5F, -2F, -0.5F, 1, 2, 1, scale);
+        hornBaseL.setRotationPoint(1F, -1.5F, -1.5F);
+        setRotation(hornBaseL, degToRad(-55F), 0F, degToRad(20F));
+        body.addChild(hornBaseL);
+
+//        hornTipL = new ModelRenderer(this, 3, 4);
+//        hornTipL.addBox(-0.5F, -0.5F, -0.5F, 1, 1, 1, 0F);
+//        hornTipL.setRotationPoint(0F, -2F, 0F);
+//        setRotation(hornTipL, 0F, 0F, 0F);
+//        hornBaseL.addChild(hornTipL);
+//
+//        hornTipR = new ModelRenderer(this, 10, 4);
+//        hornTipR.addBox(-0.5F, -4F, -0.5F, 1, 1, 1, 0F);
+//        hornTipR.setRotationPoint(0F, 0F, 0F);
+//        setRotation(hornTipR, 0F, 0F, 0F);
+//        hornBaseR.addChild(hornTipR);
     }
 
     public void render(Entity entity, float time, float swingSuppress, float f2, float headAngleY, float headAngleX, float f5) {
         super.render(entity, time, swingSuppress, f2, headAngleY, headAngleX, f5);
         setRotationAngles(time, swingSuppress, f2, headAngleY, headAngleX, f5, entity);
+
+        float s = 5F;
+
+        GL11.glPushMatrix();
+        GL11.glScalef(s, s, s);
         body.render(f5);
-        footBL.render(f5);
-        footFL.render(f5);
-        footFR.render(f5);
-        footBR.render(f5);
-        hornBaseR.render(f5);
-        hornBaseL.render(f5);
-        hornTipL.render(f5);
-        hornTipR.render(f5);
+        GL11.glPopMatrix();
     }
 
     public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5, Entity entity) {
         super.setRotationAngles(f, f1, f2, f3, f4, f5, entity);
+
+        resetRotations();
+        body.setRotationPoint(0F, 1.8F, 0F);
+
+        this.footBL.rotateAngleX = MathHelper.cos(f * 0.6662F) * 1.4F * f1;
+        this.footBR.rotateAngleX = MathHelper.cos(f * 0.6662F + (float)Math.PI) * 1.4F * f1;
+        this.footFL.rotateAngleX = MathHelper.cos(f * 0.6662F + (float)Math.PI) * 1.4F * f1;
+        this.footFR.rotateAngleX = MathHelper.cos(f * 0.6662F) * 1.4F * f1;
+    }
+
+    public void setRotation(ModelRenderer m, float x, float y, float z) {
+        m.rotateAngleX = x;
+        m.rotateAngleY = y;
+        m.rotateAngleZ = z;
+    }
+
+    public float degToRad(float degrees) {
+        return degrees * (float)Math.PI / 180 ;
+    }
+
+    public void resetRotations() {
+        setRotation(body, 0F, 0F, 0F);
+        setRotation(footBL, 0F, 0F, 0F);
+        setRotation(footFL, 0F, 0F, 0F);
+        setRotation(footFR, 0F, 0F, 0F);
+        setRotation(footBR, 0F, 0F, 0F);
+        setRotation(hornBaseR, degToRad(-55F), 0F, degToRad(-20F));
+        setRotation(hornBaseL, degToRad(-55F), 0F, degToRad(20F));
+//        setRotation(hornTipL, 0F, 0F, 0F);
+//        setRotation(hornTipR, 0F, 0F, 0F);
     }
 
 }
--- src/main/java/com/carlipoot/poropets/entity/EntityPoro.java
+++ src/main/java/com/carlipoot/poropets/entity/EntityPoro.java
@@ -1,13 +1,25 @@
 package com.carlipoot.poropets.entity;
 
+import com.carlipoot.poropets.utility.LogHelper;
 import net.minecraft.entity.EntityAgeable;
+import net.minecraft.entity.passive.EntityChicken;
 import net.minecraft.entity.passive.EntityTameable;
 import net.minecraft.world.World;
 
 public class EntityPoro extends EntityTameable {
 
+    public float scale = 5F;
+    public float size = 0.35F;
+
     public EntityPoro(World par1World) {
         super(par1World);
+        this.setSize(0.35F * scale, 0.35F * scale);
+        LogHelper.info(this.getAIMoveSpeed());
+    }
+
+    @Override
+    public float getAIMoveSpeed() {
+        return 1F;
     }
 
     @Override
